7e0fa726c6e9c910e27774e90e07c63c50ec55f8
shinharad <shinharad.github@gmail.com>
2019-06-19 10:03:00 +0900

    add lesson20

--- lesson20/app/Main.hs
+++ lesson20/app/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
-import Lib
+import           Lib
 
-import Data.List
-import qualified Data.Map as Map
-import Data.Semigroup
-import Data.Maybe
+import           Data.List
+import qualified Data.Map       as Map
+import           Data.Maybe
+import           Data.Semigroup
 
 main :: IO ()
 main = someFunc
@@ -50,10 +50,7 @@ fileToTS tvPairs = createTS times values
 -- showTVPair to render time/value pairs readable
 showTVPair :: Show a => Int -> (Maybe a) -> String
 showTVPair time (Just value) = mconcat [show time, "|", show value, "\n"]
-showTVPair time Nothing = mconcat [show time, "|NA\n"]
-
-
-
+showTVPair time Nothing      = mconcat [show time, "|NA\n"]
 
 -- Making TS an instance of Show by using zipWith and showTVPair
 instance Show a => Show (TS a) where
@@ -73,4 +70,127 @@ ts3 = fileToTS file3
 ts4 :: TS Double
 ts4 = fileToTS file4
 
---20.2. STITCHING TOGETHER TS DATA WITH SEMIGROUP AND MONOIDから
+-- insertMaybePair, a helper function for inserting (k, Maybe v) pairs
+insertMaybePair :: Ord k => Map.Map k v -> (k, Maybe v) -> Map.Map k v
+insertMaybePair myMap (_, Nothing)        = myMap
+insertMaybePair myMap (key, (Just value)) = Map.insert key value myMap
+
+combineTS :: TS a -> TS a -> TS a
+combineTS (TS [] []) ts2 = ts2
+combineTS ts1 (TS [] []) = ts1
+combineTS (TS t1 v1) (TS t2 v2) = TS completeTimes combinedValues
+  where bothTimes = mconcat [t1, t2]
+        completeTimes = [minimum bothTimes .. maximum bothTimes]
+        tvMap = foldl insertMaybePair Map.empty (zip t1 v1)
+        updatedMap = foldl insertMaybePair tvMap (zip t2 v2)
+        combinedValues = map (\v -> Map.lookup v updatedMap)
+                            completeTimes
+
+-- Making TS an instance of Semigroup
+instance Semigroup (TS a) where
+  (<>) = combineTS
+
+-- ts1 <> ts2
+
+-- Making TS an instance of Monoid
+instance Monoid (TS a) where
+  mempty = TS [] []
+  mappend = (<>)
+
+-- mconcat [ts1, ts2]
+
+tsAll :: TS Double
+tsAll = mconcat [ts1,ts2,ts3,ts4]
+
+-- 20.3. PERFORMING CALCULATIONS ON YOUR TIME SERIES
+
+-- mean to average a list of most number types
+mean :: (Real a) => [a] -> Double
+mean xs = total / count
+  where total = (realToFrac . sum) xs
+        count = (realToFrac . length) xs
+
+meanTS :: (Real a) => TS a -> Maybe Double
+meanTS (TS _ []) = Nothing
+meanTS (TS times values) = if all (== Nothing) values
+                           then Nothing
+                           else Just avg
+  where justVals = filter isJust values
+        cleanVals = map fromJust justVals
+        avg = mean cleanVals
+
+-- Listing 20.15. makeTSCompare and useful type synonyms
+type CompareFunc a = a -> a -> a
+type TSCompareFunc a = (Int, Maybe a) -> (Int, Maybe a) -> (Int, Maybe a)
+
+makeTSCompare :: Eq a => CompareFunc a -> TSCompareFunc a
+makeTSCompare func = newFunc
+  where newFunc (i1, Nothing) (i2, Nothing) = (i1, Nothing) -- pattern matching.
+        newFunc (_, Nothing) (i, val)  = (i, val)
+        newFunc (i, val) (_, Nothing) = (i, val)
+        newFunc (i1, Just val1) (i2, Just val2) =
+                                        if func val1 val2 == val1
+                                        then (i1, Just val1)
+                                        else (i2, Just val2)
+
+-- makeTSCompare max (3, Just 200) (4, Just 10)
+
+-- Listing 20.16. compareTS, a generic means of applying comparison functions to TS
+compareTS :: Eq a => (a -> a -> a) -> TS a -> Maybe (Int, Maybe a)
+compareTS func (TS [] []) = Nothing
+compareTS func (TS times values) = if all (== Nothing) values
+                                   then Nothing
+                                   else Just best
+  where pairs = zip times values
+        best = foldl (makeTSCompare func) (0, Nothing) pairs
+
+-- Listing 20.17. Trivially creating minTS and maxTS using compareTS
+minTS :: Ord a => TS a -> Maybe (Int, Maybe a)
+minTS = compareTS min
+
+maxTS :: Ord a => TS a -> Maybe (Int, Maybe a)
+maxTS = compareTS max
+
+-- minTS tsAll
+-- maxTS tsAll
+
+-- 20.4. TRANSFORMING TIME SERIES
+
+diffPair :: Num a => Maybe a -> Maybe a -> Maybe a
+diffPair Nothing _         = Nothing
+diffPair _ Nothing         = Nothing
+diffPair (Just x) (Just y) = Just (x - y)
+
+-- diffTS to take the diff of a TS
+diffTS :: Num a => TS a -> TS a
+diffTS (TS [] []) = TS [] []
+diffTS (TS times values) = TS times (Nothing:diffValues)
+  where shiftValues = tail values
+        diffValues = zipWith diffPair shiftValues values -- valuesの末尾が欠落？
+
+--movingAverageTS :: (Real a) => TS a -> Int -> TS Double
+
+meanMaybe :: (Real a) => [Maybe a] -> Maybe Double
+meanMaybe vals = if any (== Nothing) vals
+                 then Nothing
+                 else (Just avg)
+  where avg = mean (map fromJust vals)
+
+-- movingAvg calculates the moving average of a Maybe a list
+movingAvg :: (Real a) => [Maybe a] -> Int -> [Maybe Double]
+movingAvg [] n = []
+movingAvg vals n = if length nextVals == n
+                   then meanMaybe nextVals:movingAvg restVals n
+                   else []
+  where nextVals = take n vals
+        restVals = tail vals
+
+-- maTS for calculating the moving average of a TS with centering
+movingAverageTS :: (Real a) => TS a -> Int -> TS Double
+movingAverageTS (TS [] []) n = TS [] []
+movingAverageTS (TS times values) n = TS times smoothedValues
+  where ma = movingAvg values n
+        nothings = replicate (n `div` 2) Nothing
+        smoothedValues = mconcat [nothings,ma,nothings]
+
+
