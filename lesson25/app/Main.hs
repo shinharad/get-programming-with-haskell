64592ea23c79c3823bce6fd560b14325c500591b
shinharad <shinharad.github@gmail.com>
2019-06-26 22:54:58 +0900

    modify lesson25

--- lesson25/app/Main.hs
+++ lesson25/app/Main.hs
@@ -6,6 +6,10 @@ import System.Environment
 import System.Random
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Encoding as E
+import Control.Monad
 
 sampleBytes :: B.ByteString
 sampleBytes = "Hello!"
@@ -28,8 +32,12 @@ main = do
   args <- getArgs
   let fileName = head args
   imageFile <- BC.readFile fileName
-  glitched <- randomReplaceByte imageFile
---  glitched <- return imageFile -- 評価させたうえでIOで返す
+  glitched <- foldM (\bytes func -> func bytes) imageFile
+                                                [ randomReplaceByte
+                                                , randomSortSection
+                                                , randomReplaceByte
+                                                , randomSortSection
+                                                , randomReplaceByte ]
   let glitchedFileName = mconcat ["glitched_", fileName]
   BC.writeFile glitchedFileName glitched
   print "all done"
@@ -59,5 +67,26 @@ randomReplaceByte bytes = do
   charVal <- randomRIO (0, 255)
   return (replaceByte location charVal bytes)
 
+-- sortSection sorts a section of bytes in your file
+sortSection :: Int -> Int -> BC.ByteString -> BC.ByteString
+sortSection start size bytes = mconcat [before, changed, after]
+  where (before, rest) = BC.splitAt start bytes
+        (target, after) = BC.splitAt size rest
+        changed = BC.reverse (BC.sort target)
 
--- 25.2.2. Sorting random bytesから
+-- Randomizing your sortSection by using an IO action
+randomSortSection :: BC.ByteString -> IO BC.ByteString
+randomSortSection bytes = do
+  let sectionSize = 25
+  let bytesLength = BC.length bytes
+  start <- randomRIO (0, bytesLength - sectionSize)
+  return (sortSection start sectionSize bytes)
+
+sampleText :: T.Text
+sampleText = "山田太郎"
+
+sampleTextSafe :: B.ByteString
+sampleTextSafe = E.encodeUtf8 sampleText
+
+-- GHCi> TIO.putStrLn (E.decodeUtf8 sampleTextSafe)
+-- 山田太郎
