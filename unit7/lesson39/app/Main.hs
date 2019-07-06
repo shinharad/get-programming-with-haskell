11ddd22c4d5cca878e322ef2d57c6893a5139b3e
shinharad <shinharad.github@gmail.com>
2019-07-07 00:34:19 +0900

    fix lesson39

--- unit7/lesson39/app/Main.hs
+++ unit7/lesson39/app/Main.hs
@@ -10,31 +10,14 @@ import           Network.HTTP.Simple
 
 import           Lib
 
-main :: IO ()
-main = do
-  res <- httpLBS "http://news.ycombinator.com"
-  print (getResponseStatusCode res)
-
-main' :: IO ()
-main' = do
-  let request = buildRequest (BC.pack "") (BC.pack "") (BC.pack "") (BC.pack "") -- example
-  response <- httpLBS request
-  let status = getResponseStatusCode response
-  if status == 200
-    then do
-      print "saving request to file"
-      let jsonBody = getResponseBody response
-      L.writeFile "data.json" jsonBody
-    else print "request failed with error"
-
 buildRequest :: BC.ByteString -> BC.ByteString -> BC.ByteString -> BC.ByteString -> Request
 buildRequest token host method path =
-  setRequestMethod method $
-  setRequestHost host $
-  setRequestHeader "token" [token] $
-  setRequestPath path $
-  setRequestSecure True $
-  setRequestPort 443 defaultRequest
+  setRequestMethod method
+    $ setRequestHost host
+    $ setRequestHeader "token" [token]
+    $ setRequestPath path
+    $ setRequestSecure True
+    $ setRequestPort 443 defaultRequest
 
 -- こう書くのと同じこと
 buildRequest' :: BC.ByteString -> BC.ByteString -> BC.ByteString -> BC.ByteString -> Request
@@ -44,3 +27,22 @@ buildRequest' token host method path =
        in let state3 = setRequestPath path state2
            in let state4 = setRequestHeader "token" [token] state3
                in setRequestHost host state4
+
+myToken = "" :: BC.ByteString
+host = "www.ncei.noaa.gov" :: BC.ByteString
+path = "access/services/data/v1?dataset=global-summary-of-the-year&dataTypes=DP01,DP05,DP10,DSND,DSNW,DT00,DT32,DX32,DX70,DX90,SNOW,PRCP&stations=ASN00084027&startDate=1952-01-01&endDate=1970-12-31&includeAttributes=true&format=json" :: BC.ByteString
+
+request = buildRequest myToken host "GET" path :: Request
+
+main :: IO ()
+main = do
+  response <- httpLBS request
+  let status = getResponseStatusCode response
+  if status == 200
+    then do
+      print "saving request to file"
+      let jsonBody = getResponseBody response
+      L.writeFile "data.json" jsonBody
+    else print "request failed with error"
+
+
