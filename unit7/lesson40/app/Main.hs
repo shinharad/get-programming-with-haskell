e0310bd95cc435cd93ee0c8ac80bb9e7d3935c91
shinharad <shinharad.github@gmail.com>
2019-07-08 10:22:45 +0900

    add lesson40

---
+++ unit7/lesson40/app/Main.hs
@@ -0,0 +1,95 @@
+module Main where
+
+import           Data.Aeson
+import           Data.ByteString.Lazy       as B
+import           Data.ByteString.Lazy.Char8 as BC
+import           Data.Text                  as T
+import           GHC.Generics
+
+main :: IO ()
+main = print "hi"
+
+data Book =
+  Book
+    { title  :: T.Text
+    , author :: T.Text
+    , year   :: Int
+    }
+  deriving (Show, Generic)
+
+instance FromJSON Book
+
+instance ToJSON Book
+
+myBook :: Book
+myBook = Book {author = "Will Kurt", title = "Learn Haskell", year = 2017}
+
+myBookJSON :: BC.ByteString
+myBookJSON = encode myBook
+
+wrongJSON :: BC.ByteString
+wrongJSON = "{\"writer\":\"Emil Cioran\",\"title\":\"A Short History of Decay\",\"year\"=1949}"
+
+bookFromWrongJSON :: Maybe Book
+bookFromWrongJSON = decode wrongJSON
+
+--
+-- GHCi> eitherDecode collectJSON :: Either String Book
+-- Right (Book {title = "Learn Haskell", author = "Will Kurt", year = 2017})
+--
+collectJSON :: BC.ByteString
+collectJSON = "{\"year\":2017,\"author\":\"Will Kurt\",\"title\":\"Learn Haskell\"}"
+
+-- Writing your own instances of FromJSON and ToJSON
+sampleError :: BC.ByteString
+sampleError = "{\"message\":\"oops!\",\"error\": 123}"
+
+-- "error"というプロパティが予約語とかぶる場合
+data ErrorMessage =
+  ErrorMessage
+    { message :: T.Text
+    , error   :: Int
+    }
+  deriving (Show)
+
+--
+-- This operator takes an Object (your JSON object) and some text and returns a value parsed into a context.
+-- For example, this line of code is trying to parse the message field from your JSON object:
+--   v .: "message"
+--
+-- GHCi> :t (.:)
+-- (.:)
+--   :: FromJSON a =>
+--      Object -> Text -> aeson-1.4.2.0:Data.Aeson.Types.Internal.Parser a
+--
+instance FromJSON ErrorMessage where
+  parseJSON (Object v) = ErrorMessage <$> v .: "message" <*> v .: "error"
+
+instance ToJSON ErrorMessage where
+  toJSON (ErrorMessage message errorCode) = object ["message" .= message, "error" .= errorCode]
+
+--
+-- GHCi> ErrorMessage <$> exampleMessage <*> exampleError
+-- Just (ErrorMessage {message = "Opps", error = 123})
+exampleMessage :: Maybe T.Text
+exampleMessage = Just "Opps"
+
+exampleError :: Maybe Int
+exampleError = Just 123
+
+sampleErrorMessage :: Maybe ErrorMessage
+sampleErrorMessage = decode sampleError
+
+--
+-- GHCi> encode anErrorMessage
+-- "{\"error\":0,\"message\":\"Everything is Okay\"}"
+--
+-- GHCi> eitherDecode "{\"error\":0,\"message\":\"Everything is Okay\"}" :: Either String ErrorMessage
+-- Right (ErrorMessage {message = "Everything is Okay", error = 0})
+--
+anErrorMessage :: ErrorMessage
+anErrorMessage = ErrorMessage "Everything is Okay" 0
+
+-- 40.4. PUTTING IT ALL TOGETHER: READING YOUR NOAA DATA から
+
+
